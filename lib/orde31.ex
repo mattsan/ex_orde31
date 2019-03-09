be0db9c657daa62216c07fa549ec8ec667f473de
MATSUMOTO EIji <e-matsumoto@esm.co.jp>
2019-03-09 15:27:44 +0900

    initial commit

---
+++ lib/orde31.ex
@@ -0,0 +1,18 @@
+defmodule Orde31 do
+  @moduledoc """
+  Documentation for Orde31.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Orde31.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/orde31_test.exs
@@ -0,0 +1,8 @@
+defmodule Orde31Test do
+  use ExUnit.Case
+  doctest Orde31
+
+  test "greets the world" do
+    assert Orde31.hello() == :world
+  end
+end
