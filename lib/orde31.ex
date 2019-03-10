2304a1a4da76f9c79ae75fbf49bee91b7df4ff99
Eiji MATSUMOTO <e.mattsan@gmail.com>
2019-03-10 15:05:28 +0900

    上限までの個数から下限までの個数を引く方法で計算した

--- lib/orde31.ex
+++ lib/orde31.ex
@@ -1,55 +1,115 @@
 defmodule Orde31 do
+  @moduledoc """
+  [オフラインリアルタイムどう書くE31「ぐるぐる数」](http://nabetani.sakura.ne.jp/hena/orde31rotnum/) を Elixir で解いた。
+  """
+
+  @doc """
+  解答を返す
+
+  ## Examples
+
+      iex> Orde31.solve("4,1313,3012")
+      "12"
+
+      iex>  Orde31.solve("10,100,110")
+      "0"
+  """
+  @spec solve(String.t()) :: String.t()
   def solve(input) do
-    [d_str, x_str, y_str] = String.split(input, ",")
-    d = String.to_integer(d_str)
-    x = String.to_integer(x_str, d)
-    y = String.to_integer(y_str, d)
+    [base_str, x_str, y_str] = String.split(input, ",")
 
-    gurugurus(x, y, d)
-    |> Enum.count()
-    |> inspect()
+    base = String.to_integer(base_str)
+    x = String.to_integer(x_str, base)
+    y = String.to_integer(y_str, base)
+
+    calc(x, y, base)
+    |> Integer.to_string()
   end
 
-  def gurugurus(x, y, d) do
-    gs =
-      if guruguru?(x, d) do
-        [x]
-      else
-        []
-      end
-    gurugurus(x, y, d, gs)
+  @doc """
+  `base` 進数で `x` 以上 `y` 以下のぐるぐる数の数を数える
+
+  - `x` 対象の下限
+  - `y` 対象の上限
+  - `base` 基数
+  """
+  @spec calc(integer, integer, integer) :: integer
+  # 2 進数の場合は全部ぐるぐる数
+  def calc(x, y, 2), do: y - x + 1
+
+  # y 以下のぐるぐる数から x 以下のぐるぐる数の数を引く
+  # x もぐるぐる数の場合は x も数に加える
+  def calc(x, y, base) do
+    d = if guruguru?(x, base), do: 1, else: 0
+    count(y, base) - count(x, base) + d
   end
 
-  def gurugurus(x, y, d, gs) do
-    next_x = next_guruguru(x, d)
-    if next_x <= y do
-      gurugurus(next_x, y, d, [next_x | gs])
+  @doc """
+  `max` 以下の `base` 進数のぐるぐる数を数える
+
+  - `max` ぐるぐる数の上限
+  - `base` 基数
+  """
+  @spec count(integer, integer) :: integer
+  def count(max, base) do
+    count(max, base, [1], 1)
+  end
+
+  @doc """
+  `digits` 以降で `max` 以下の `base` 進数のぐるぐる数を数える
+
+  - `max` ぐるぐる数の上限
+  - `base` 基数
+  - `digits` ぐるぐる数を `base` 進数で表した時の各桁の数値のリスト（先頭が最下位桁、末尾が最上位桁の順）
+  - `c` 元になるカウント
+  """
+  @spec count(integer, integer, [integer], integer) :: integer
+  def count(max, base, digits, c) do
+    n = digits |> Enum.reverse() |> Integer.undigits(base)
+
+    if n <= max do
+      count(max, base, next(digits, base), c + 1)
     else
-      gs
+      c
     end
   end
 
-  def next_guruguru(x, d) do
-    x1 = x + 1
-    if guruguru?(x1, d) do
-      x1
+  @doc """
+  次のぐるぐる数を取得する
+
+  - `digits` ぐるぐる数を `base` 進数で表した時の各桁の数値のリスト（先頭が最下位桁、末尾が最上位桁の順）
+  - `base` 基数
+  """
+  @spec next([integer], integer) :: [integer]
+  def next(digits, base)
+  def next([1], 2),                                     do: [0, 1]
+  def next([digit], base) when digit == base - 1,       do: [1, 1]
+  def next([digit], _),                                 do: [digit + 1]
+
+  def next([d0, d0 | digits], base) when d0 < base - 1, do: [d0 + 1, d0 | digits]
+  def next([0, d1 | digits], _),                        do: [d1, d1 | digits]
+
+  def next([_, d1 | digits], base) do
+    [d | _] = ds = next([d1 | digits], base)
+
+    if d == base - 1 do
+      [0 | ds]
     else
-      next_guruguru(x1, d)
+      [d | ds]
     end
   end
 
-  def guruguru?(x, d) do
-    [d0 | ds] = Integer.digits(x, d)
-    ds
-    |> Enum.reduce(d0, fn
-      x1, x1 ->
-        x1
+  @doc """
+  `n` が `base` 進数のぐるぐる数か否かを返す
+  """
+  @spec guruguru?(integer, integer) :: boolean
+  def guruguru?(n, base) do
+    [first_digit | digits] = Integer.digits(n, base)
 
-      x1, x0 when rem(x0 + 1, d) == x1 ->
-        x1
-
-      _, _ ->
-        false
+    !!Enum.reduce_while(digits, first_digit, fn
+      digit, digit -> {:cont, digit}
+      digit, prev when rem(prev + 1, base) == digit -> {:cont, digit}
+      _, _ -> {:halt, false}
     end)
   end
 end
